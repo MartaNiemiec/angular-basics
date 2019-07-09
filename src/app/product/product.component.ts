a588b5822c9a4161450f6bcb8d314c3359b8d98c
Marta Niemiec <niemiec.marta@hotmail.com>
2019-07-10 00:11:31 +0200

    add products.service and Subscription

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
+import { ProductsService } from '../products.service';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
 
-  constructor() { }
+export class ProductComponent implements OnInit {
   @Input() productName: string; // productName can be receive from outside
   @Output() productClicked = new EventEmitter()  // @Output() to pass data to the outside
 
+  constructor(private productsService: ProductsService) { }
+
   ngOnInit() {
   }
+
   onClicked() {
-    this.productClicked.emit();
+    // this.productClicked.emit();
+    this.productsService.deleteProduct(this.productName);
   }
 
 }
